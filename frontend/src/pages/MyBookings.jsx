eb02e6424c82cec8d35014a7b2a3bd7792a9f3ff
prabhanjaningle <prabhanjan797@gmail.com>
2026-02-21 15:29:42 +0530

    Updated API for production deployment

--- frontend/src/pages/MyBookings.jsx
+++ frontend/src/pages/MyBookings.jsx
@@ -64,7 +64,7 @@ function MyBookings() {
       <div style={styles.list}>
         {bookings.map((booking) => (
           <div key={booking._id} style={styles.card}>
-            <h4>Expert ID: {booking.expertId?.name}</h4>
+            <h4>Expert: {booking.expertId?.name}</h4>
             <p>Category :{booking.expertId?.category}</p>
             <p>Date: {booking.date}</p>
             <p>Time: {booking.time}</p>
